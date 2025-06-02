f68485d09b6943130abedf240b71925f51137eb3
abdullah-hassan1 <abdullahhassan1@protonmail.com>
2025-06-02 20:05:21 +0500

    n/a

--- algorithm/summarization.py
+++ algorithm/summarization.py
@@ -4,8 +4,7 @@ import numpy as np
 from nltk.cluster.util import cosine_distance
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# nltk.download('punkt')
-# nltk.download('stopwords')
+
 nltk.download('all')
 stopwords = nltk.corpus.stopwords.words('english')
 
